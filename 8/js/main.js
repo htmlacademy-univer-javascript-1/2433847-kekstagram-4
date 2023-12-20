fb5f912d5f9eca1e9d406ab743786ae6247cbaf3
Keks <keks@htmlacademy.ru>
2023-12-20 21:19:37 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,58 @@
+import {photos, descriptions, messages, names} from './data.js';
+
+import {randomInteger} from './util.js';
+
+import {installPhotosInGallery} from './photos.js';
+
+installPhotosInGallery(photos);
+
+const COUNT_PHOTO =  25;
+
+const Comments = {
+  MIN: 0,
+  MAX: 30
+};
+const Ids = {
+  MIN: 1,
+  MAX: 100
+};
+
+const Likes = {
+  MIN: 15,
+  MAX: 200
+};
+
+const addComment = () => ({
+  id: randomInteger(Ids.MIN, Ids.MAX),
+  avatar: `img/avatar-${randomInteger(1, 6)}}.svg`,
+  message: messages[randomInteger(0, messages.length - 1)],
+  name: names[randomInteger(0, names.length - 1)]
+});
+
+const generateComments = () => {
+  const commentsArray = [];
+  const commentsCount = randomInteger(Comments.MIN, Comments.MAX);
+  for (let i = 0; i < commentsCount; i++) {
+    commentsArray.push(addComment());
+  }
+  return Comments;
+};
+
+
+const addPhoto = (index) => ({
+  id: index + 1,
+  url: `photos/${index + 1}.jpg`,
+  description: descriptions[randomInteger(0, descriptions.length-1)],
+  likes: randomInteger(Likes.MIN, Likes.MAX),
+  comments: generateComments()
+});
+
+const addPhotos = () => {
+  for (let i = 0; i < COUNT_PHOTO; i++) {
+    photos.push(addPhoto(i));
+  }
+};
+
+addPhotos();
+
+
